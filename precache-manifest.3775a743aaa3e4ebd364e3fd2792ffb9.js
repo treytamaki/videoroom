b375dcf56d72e56f44b84c73647e5fa12ffab34a
khaittran <erick84khai@gmail.com>
2020-06-04 21:35:49 -0700

    Updates

---
+++ precache-manifest.3775a743aaa3e4ebd364e3fd2792ffb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5aeabe2ea3f2efd806313222b77a82",
+    "url": "/videoroom/index.html"
+  },
+  {
+    "revision": "d14b870c3bdbac4eab6d",
+    "url": "/videoroom/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e23347f5bf7959860c8",
+    "url": "/videoroom/static/js/2.5102f090.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videoroom/static/js/2.5102f090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14b870c3bdbac4eab6d",
+    "url": "/videoroom/static/js/main.9e5a4631.chunk.js"
+  },
+  {
+    "revision": "8baf32a3ef328bcc6a37",
+    "url": "/videoroom/static/js/runtime-main.98e31fbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/videoroom/static/media/logo.5d5d9eef.svg"
+  }
+]);
